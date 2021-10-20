e933fa86fda97891b766992de9e69da151f3864d
Vinoth Varatharaj <v.vinoth01@gmail.com>
2021-10-20 13:21:39 +0530

    Minor changes on table data rendering from a json

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,19 +2,25 @@ import React from "react";
 
 export const Header = () => {
   return (
-    <nav
-      className="navbar is-dark" role="navigation"
-    >
+    <nav className="navbar is-dark" role="navigation">
       <div className="navbar-brand">
         <div className="navbar-item">
           <h1 className="is-size-3 is-uppercase">Toll India</h1>
         </div>
       </div>
       <div className="navbar-end">
-        <div className="navbar-item">
-          <a href="" className="is-primary is-size-4">
-            Github
-          </a>
+        <div className="navbar-item has-dropdown is-hoverable">
+          <a className="navbar-link">Kiran's Project</a>
+            <div className="navbar-dropdown is-right">
+              <a className="navbar-item" href="https://kiranhpe.github.io/covid19-react/#/"
+                target="_blank">
+                Covid-19 Statistics
+              </a>
+              <a className="navbar-item" href="http://toll-india.herokuapp.com/api/toll/66"
+                target="_blank">
+                Toll India API
+              </a>
+            </div>
         </div>
       </div>
     </nav>
